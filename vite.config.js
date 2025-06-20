7d2539e50271c635520e58df99aa305bda6a8ecd
Kainat Sajid <kainat.sajidh@gmail.com>
2025-06-20 14:02:41 -0400

    deploy updates

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const repoName = 'my-portfolio'
+
 // https://vite.dev/config/
 export default defineConfig({
+  base: `/${repoName}/`,
   plugins: [react()],
 })
